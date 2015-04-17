5c6255666d79de5ef4fccf17a161dab6057f55cb
suakig <suakig@gmail.com>
2015-04-18 03:59:28 +0900

    add .gitignore

---
+++ Assets/Scripts/CollisionLimit.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// 反発回数制限クラス
+/// 指定時間内に特定回数以上オブジェクトに接触処理させないためのクラス
+/// オブジェクトの名前を利用し、接触回数を制限する。
+/// 同じ名前のオブジェクトは区別されない
+/// 時間が経過すると削除されるQueueを作成することで処理している
+/// </summary>
+public class CollisionLimit : SingletonMonoBehaviour<CollisionLimit>
+{
+	public float deleteTime = 1.0f;
+	public int maxCount = 2;
+
+	/// <summary>
+	/// すでにある場合は削除する。
+	/// </summary>
+	void Awake()
+	{
+		if(this != Instance)
+		{
+			Destroy(this);
+			return;
+		}
+    }  
+
+	/// <summary>
+	/// アクティブになったら初期化を行う
+	/// </summary>
+	void OnEnable()
+	{
+		Clear ();
+	}
+
+	//二つの接触したオブジェクトの名前を入れる
+	private class TwoCollisionObjectName
+	{
+		private string obj1;
+		private string obj2;
+
+		public TwoCollisionObjectName(string obj1, string obj2)
+		{
+			this.obj1 = obj1;
+			this.obj2 = obj2;
+		}
+
+		public bool isEquals(TwoCollisionObjectName checkdata)
+		{
+			if (this.obj1 == checkdata.obj1) {
+				if (this.obj2 == checkdata.obj2) {
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+	private Queue<TwoCollisionObjectName> queue = new Queue<TwoCollisionObjectName>();
+
+	/// <summary>
+	/// 同じデータの個数を数えて返す
+	/// </summary>
+	/// <param name="data">Data.</param>
+	private int Count(TwoCollisionObjectName data)
+	{
+		int count = 0;
+		foreach(TwoCollisionObjectName index in queue)
+		{
+			if (data.isEquals(index)) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	/// <summary>
+	/// 時間が経過したら古いデータを削除する
+	/// </summary>
+	private void Delete()
+	{
+		queue.Dequeue ();
+	}
+
+	/// <summary>
+    /// 当たっているかどうか判定する
+    /// Queueにデータをため、同じものが指定数以上あるかチェックする
+	/// </summary>
+	/// <param name="data">Data.</param>
+    public bool isHit(string obj1, string obj2)
+	{
+		TwoCollisionObjectName data = new TwoCollisionObjectName (obj1, obj2);
+		if (Count (data) >= maxCount) {
+			return false;
+		}
+
+		queue.Enqueue(data);
+		Invoke ("Delete", deleteTime);
+		return true;
+	}
+
+	/// <summary>
+	/// データのリセット
+	/// </summary>
+	public void Clear()
+	{
+		queue.Clear ();
+	}
+}
---
+++ Assets/Scripts/SampleObject.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SampleObject : MonoBehaviour {
+
+    public enum Way
+    {
+        Up      =   1,
+        Down    =   2,
+        Left    =   4,
+        Right   =   8
+    }
+
+    public Way way = Way.Up;
+    private float speed = 30;
+
+    void Start ()
+    {
+        WayInit ();
+    }
+
+    /// <summary>
+    /// 初期移動方向の初期化
+    /// </summary>
+    void WayInit()
+    {
+        switch(way){
+        case Way.Up:
+            rigidbody.AddForce (Vector3.up);
+            break;
+        case Way.Down:
+            rigidbody.AddForce (Vector3.down);
+            break;
+        case Way.Left:
+            rigidbody.AddForce (Vector3.left);
+            break;
+        case Way.Right:
+            rigidbody.AddForce (Vector3.right);
+            break;
+        }
+    }
+
+    /// <summary>
+    /// 等速移動
+    /// </summary>
+    void FixedUpdate()
+    {
+        if (rigidbody.velocity.magnitude == 0.0f) {
+            WayInit ();
+            return;
+        }
+
+        float ratio = speed / rigidbody.velocity.magnitude;
+        rigidbody.velocity *= ratio;
+    }
+
+    /// <summary>
+    /// Enter時に判定を行う
+    /// </summary>
+    /// <param name="collision">Collision.</param>
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "RangeBox") {
+            return;
+        }
+
+        if (CollisionLimit.Instance.isHit (collision.gameObject.name, gameObject.name)) {
+            Debug.Log ("Hit " + collision.gameObject.name + " and " + gameObject.name);
+        } else {
+            Debug.Log ("Not " + collision.gameObject.name + " and "+ gameObject.name);
+        }
+    }
+}
